693f6d216d60238f8ce0e8d9f49589f321f8868f
Jamie Le Souef <jamielesouef@icloud.com>
2024-04-10 14:09:46 +1000

    start of addressing modes

--- SwiftNES/CPU/6502.swift
+++ SwiftNES/CPU/6502.swift
@@ -8,12 +8,21 @@
 import Foundation
 
 final class CPU {
-  private (set) var pc: UInt16 = 0x0000
+  typealias MemoryAddress = UInt16
+  typealias Opperand = UInt8
+  typealias Oppcode = UInt8
+  
+  private (set) var pc: MemoryAddress = 0x0000
   private (set) var registers: Registers = Registers()
   
   private var mem: [UInt8] = .init(repeating: 0, count: 0xFFFF)
   
   private var loop = true //temport until flags
+  
+  enum AddressingIndex {
+    case X
+    case Y
+  }
   
   enum CPUError: Error {
     case invalidOpcode(String)
@@ -27,7 +36,7 @@ final class CPU {
       try dispatch(oppcode)
     }
     
-    func dispatch(_ opcode: UInt8) throws {
+    func dispatch(_ opcode: Oppcode) throws {
       switch opcode {
       case 0xAA: self.TAX()
       case 0xA9: self.LDA()
@@ -42,7 +51,65 @@ final class CPU {
   }
 }
 // MARK: Memory Access
+
+enum AddressingMode {
+  case immediate
+  case zeroPage
+  case zeroPageX
+  case zeroPageY
+  case absolute
+  case absoluteX
+  case absoluteY
+  case indirect
+  case indirectIndexed
+  case relative
+}
+
 extension CPU {
+  
+  private func getRegisterValue(for register: AddressingIndex) -> UInt8 {
+    switch register {
+    case .X: return registers.X
+    case .Y: return registers.Y
+    }
+  }
+  
+  private func getZeroPage(for register: AddressingIndex) -> MemoryAddress {
+    let operand = readMem(at: pc)
+    var registerValue: UInt8 = getRegisterValue(for: register)
+    
+    return MemoryAddress(operand.addingReportingOverflow(registerValue).partialValue)
+  }
+  
+  private func getAbsolute(for register: AddressingIndex) -> MemoryAddress {
+    let operand = readMem16(at: pc)
+    var registerValue: MemoryAddress = MemoryAddress(getRegisterValue(for: register))
+    
+    return MemoryAddress(operand.addingReportingOverflow(registerValue).partialValue)
+  }
+  
+  func getOpperandAddress(for mode: AddressingMode) -> MemoryAddress {
+    switch mode {
+    case .immediate:
+      return pc
+    case .zeroPage:
+      return MemoryAddress(readMem(at: pc))
+    case .zeroPageX: return getZeroPage(for: .X)
+    case .zeroPageY: return getZeroPage(for: .Y)
+    case .relative:
+      <#code#>
+    case .absolute:
+      return readMem16(at: pc)
+    case .absoluteX:
+      <#code#>
+    case .absoluteY:
+      <#code#>
+    case .indirect:
+      <#code#>
+    case .indirectIndexed:
+      <#code#>
+    }
+  }
   
   func load(program: [UInt8]) {
     self.mem.insert(contentsOf: program, at: 0x8000)
@@ -55,15 +122,17 @@ extension CPU {
     pc = readMem16(at: 0xFFFC)
   }
   
-  func readMem(at address: UInt16) -> UInt8 {
+ 
+  
+  func readMem(at address: MemoryAddress) -> Opperand {
     return mem[address]
   }
   
-  func writeMem(at address: UInt16, value: UInt8) {
+  func writeMem(at address: MemoryAddress, value: Opperand) {
     mem[address] = value
   }
   
-  func readMem16(at address: UInt16) -> UInt16 {
+  func readMem16(at address: MemoryAddress) -> MemoryAddress {
     return UInt16(mem[address]) | UInt16(mem[address + 1]) << 8
   }
   
