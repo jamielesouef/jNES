fa565e179cc3a3381ffdfdd948c47b9f0112b871
Jamie Le Souef <jamielesouef@icloud.com>
2024-04-25 10:11:17 +1000

    Have an rendering buffer!

---
+++ SwiftNES/ScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenView.swift
+//  SwiftNES
+//
+//  Created by Jamie Le Souef on 25/4/2024.
+//
+
+import SwiftUI
+
+struct ScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenView()
+}
